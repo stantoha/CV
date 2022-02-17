8426882ba0b98cd7e1b8af287dcfd19dc7dd6ce4
stantoha <78742661+stantoha@users.noreply.github.com>
2022-02-17 21:13:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+let about=document.querySelector('.about')
+let plusButton1=document.querySelector('.plusButton1')
+
+plusButton1.onclick = function() {
+    about.classList.remove('visible');
+    about.classList.toggle('unvisible');
+};
+
+
+
+let skills=document.querySelector('.skills')
+let plusButton2=document.querySelector('.plusButton2')
+
+plusButton2.onclick = function() {
+    skills.classList.remove('visible');
+    skills.classList.toggle('unvisible');
+};
+
+
+
+let languages=document.querySelector('.languages')
+let plusButton3=document.querySelector('.plusButton3')
+
+plusButton3.onclick = function() {
+    languages.classList.remove('visible');
+    languages.classList.toggle('unvisible');
+};
+
+
+
+let ref=document.querySelector('.ref')
+let plusButton4=document.querySelector('.plusButton4')
+
+plusButton4.onclick = function() {
+    ref.classList.remove('visible');
+    ref.classList.toggle('unvisible');
+};
+
+
+
+let work=document.querySelector('.work')
+let plusButton5=document.querySelector('.plusButton5')
+
+plusButton5.onclick = function() {
+    work.classList.remove('visible');
+    work.classList.toggle('unvisible');
+};
+
+
+
+let education=document.querySelector('.education')
+let plusButton6=document.querySelector('.plusButton6')
+
+plusButton6.onclick = function() {
+    education.classList.remove('visible');
+    education.classList.toggle('unvisible');
+};
+
+
+
+let interests=document.querySelector('.interests-menu')
+let plusButton7=document.querySelector('.plusButton7')
+
+plusButton7.onclick = function() {
+    interests.classList.remove('visible');
+    interests.classList.toggle('unvisible');
+};
